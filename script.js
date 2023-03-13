506a957228dfc1cb3d0a6ba95f915e5bb6a28337
nmlkk <116718957+allgww@users.noreply.github.com>
2023-03-13 22:22:29 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let form = document.forms.user
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    let st = {
+        id: Math.random(),
+    }
+
+    let fm = new FormData(form)
+    fm.forEach((value, key) => {
+        st[key] = value
+    })
+}
+
+function reload(arr, pl) {
+    pl.innerHTML = ""
+    for(let item of arr){
+        //th
+        // let tRow = document.createElement("tr")
+        // let id = document.createElement('th')
+        // let push_wallet = document.createElement('th')
+        // let catg = document.createElement('th')
+        // let money_tran = document.createElement('th')
+        // let when_tr = document.createElement('th')
+        //td
+        let tRow_td = document.createElement('tr')
+        let number = document.createElement('td')
+        let card = document.createElement('td')
+        let catg_td = document.createElement('td')
+        let money_tran_tg = document.createElement('td')
+        let when_td = document.createElement('td')
+
+        // id.innerHTML = "ID"
+        // push_wallet.innerHTML = 'Отправлено с кошелька' 
+        // catg.innerHTML = 'Категория'
+        // money_tran.innerHTML = 'Сумма транзации'
+        // when_tr.innerHTML = "Когда"
+
+        number.innerHTML = arr.indexOf(item)
+        card.innerHTML = item.wallet
+        catg_td.innerHTML = item.puschased
+        money_tran_tg.innerHTML = item.money
+        when_td.innerHTML = item.when
+
+        tRow_td.append(number,card,catg_td,money_tran_tg,when_td)
+        pl.append(tRow_td)
+    }
+
+}
